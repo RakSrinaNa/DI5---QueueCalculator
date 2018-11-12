faab5ecdb0bd2fbf86f1143b1a979abff7e55b38
Thomas COUCHOUD <zerderr@gmail.com>
2018-11-12 15:58:07 +0100

    Improve perfs

--- src/main/java/fr/mrcraftcod/queue/Computation.java
+++ src/main/java/fr/mrcraftcod/queue/Computation.java
@@ -1,8 +1,11 @@
 package fr.mrcraftcod.queue;
 
+import java.util.HashMap;
 import java.util.Objects;
 
 class Computation{
+	
+	private static final HashMap<Integer, Integer> facts = new HashMap<>();
 	
 	static QueueResult compute(QueueInput input) throws BadInputException{
 		if(Objects.isNull(input.getS()) || input.getS() < 1){
@@ -25,12 +28,16 @@ class Computation{
 		if(input.getLimit() != null){
 			if(input.getS() == 1){
 				//M|M|1|K
+				
+				final double rhoLimit = Math.pow(rho, input.getLimit());
+				final double rhoLimit1 = Math.pow(rho, input.getLimit() + 1);
+				
 				double q0;
 				if(rho == 1){
 					q0 = 1.0 / (input.getLimit() + 1);
 				}
 				else{
-					q0 = ((1.0 - rho)) / (1.0 - Math.pow(rho, input.getLimit() + 1));
+					q0 = (1.0 - rho) / (1.0 - rhoLimit1);
 				}
 				
 				double l;
@@ -38,15 +45,12 @@ class Computation{
 					l = input.getLimit() / 2.0;
 				}
 				else{
-					l = rho * (1.0 - ((input.getLimit() + 1) * Math.pow(rho, input.getLimit())) + (input.getLimit() * Math.pow(rho, input.getLimit() + 1))) / ((1.0 - rho) * (1.0 - Math.pow(rho, input.getLimit() + 1)));
+					l = rho * (1.0 - ((input.getLimit() + 1) * rhoLimit) + (input.getLimit() * rhoLimit1)) / ((1.0 - rho) * (1.0 - rhoLimit1));
 				}
 				
 				double lq = l - (1 - q0);
-				
 				double w = 1.0 / (input.getMu() - input.getLambda());
-				
-				double wq = (input.getLambda() / input.getMu()) * w;
-				
+				double wq = w * input.getLambda() / input.getMu() * w;
 				double ref = q0 * (Math.pow(input.getS(), input.getS()) * Math.pow(rho, input.getS())) / (fact(input.getS()));
 				
 				return new QueueResult(lq, l, wq, w, ref);
@@ -62,15 +66,16 @@ class Computation{
 			}
 			
 			//Q0
+			final double powS = Math.pow(rho * input.getS(), input.getS());
 			double sumQ0 = 0;
 			for(int i = 0; i < input.getS(); i++){
 				sumQ0 += Math.pow(rho * input.getS(), i) / fact(i);
 			}
-			sumQ0 += Math.pow(rho * input.getS(), input.getS()) / (fact(input.getS()) * (1 - rho));
+			sumQ0 += powS / (fact(input.getS()) * (1 - rho));
 			
-			double q0 = 1.0 / (sumQ0);
+			double q0 = 1.0 / sumQ0;
 			
-			double lq = q0 * ((Math.pow(rho * input.getS(), input.getS()) * rho) / (fact(input.getS()) * Math.pow(1 - rho, 2)));
+			double lq = q0 * ((powS * rho) / (fact(input.getS()) * Math.pow(1 - rho, 2)));
 			double wq = lq / input.getLambda();
 			double w = wq + 1 / input.getMu();
 			double l = input.getLambda() * w;
@@ -80,9 +85,11 @@ class Computation{
 	}
 	
 	private static int fact(int f){
-		if(f <= 1){
-			return 1;
-		}
-		return f * fact(f - 1);
+		return facts.computeIfAbsent(f, ff -> {
+			if(ff <= 1){
+				return 1;
+			}
+			return ff * fact(ff - 1);
+		});
 	}
 }
